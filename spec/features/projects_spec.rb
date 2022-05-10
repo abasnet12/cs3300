4bc28945e5ded18f3be01d6d9c9017df8645e532
Ambar <abasnet@uccs.edu>
2022-05-10 22:57:10 +0000

    Final Project

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "Projects", type: :feature do
     before(:each) do
       visit new_project_path
       signInFields
-      within(all("form")[1]) do
+      within("form") do
         fill_in "Title", with: "Test title"
       end
     end
@@ -40,14 +40,14 @@ RSpec.feature "Projects", type: :feature do
     end
 
     scenario "should be successful" do
-      within(all("form")[1]) do
+      within("form") do
         fill_in "Description", with: "New description content"
       end
       click_button "Update Project"
       expect(page).to have_content("Project was successfully updated")
     end
     scenario "should fail" do
-      within(all("form")[1]) do
+      within("form") do
         fill_in "Description", with: ""
       end
       click_button "Update Project"
